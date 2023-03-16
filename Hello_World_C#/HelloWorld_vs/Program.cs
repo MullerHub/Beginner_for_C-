9a823ce9fc19f14766978be98b631ea5084a8ad7
Murilo Muller <murilomuller@protonmail.com>
2023-03-16 17:51:16 -0300

    Hello_World_In_C#

---
+++ Hello_World_C#/HelloWorld_vs/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HelloWorld_vs
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello-World em C# usando vs 2019");
+        }
+    }
+}
