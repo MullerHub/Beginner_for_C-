9f8431aadccee39234dd1844d991fc0e5c5cb9e1
Murilo Muller <murilomuller@protonmail.com>
2023-03-28 14:57:23 -0300

    Formatar datas adicionado

--- Cogidos_Isolados/Fibonacci.cs
+++ Cogidos_Isolados/Fibonacci.cs
@@ -3,7 +3,9 @@ var fibonacciNumbers = new List<int> { 1, 1 };
 while (fibonacciNumbers.Count <= 20)
 {
   var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+
   var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
 
   fibonacciNumbers.Add(previous + previous2);
 }
