d90dd19c675644729dfd64d19ce671e0290d75b1
Murilo Muller <murilomuller@protonmail.com>
2023-03-28 19:32:15 -0300

    Exercicios de Generics valores de um triangulo adicionado

--- Cogidos_Isolados/Triangulo.Generics.Program.cs
+++ Cogidos_Isolados/Triangulo.Generics.Program.cs
@@ -2,18 +2,18 @@
 
 namespace Softblue
 {
-	class Program
-	{
-		static void Main()
-		{
-			// Cria um triângulo e define seus pontos.
-			Triangulo<int> t1 = new Triangulo<int>();
-			t1.DefinirPontos(0, 0, 0, 0, 3, 5, 2, 2, 4);
-			Console.WriteLine(t1);
+  class Program
+  {
+    static void Main()
+    {
+      // Cria um triângulo e define seus pontos.
+      Triangulo<int> t1 = new Triangulo<int>();
+      t1.DefinirPontos(0, 0, 0, 0, 3, 5, 2, 2, 4);
+      Console.WriteLine(t1);
 
-			// Cria outro triângulo com os mesmos pontos de T1.
-			Triangulo<int> t2 = new Triangulo<int>(t1.P1, t1.P2, t1.P3);
-			Console.WriteLine(t2);
-		}
-	}
+      // Cria outro triângulo com os mesmos pontos de T1.
+      Triangulo<int> t2 = new Triangulo<int>(t1.P1, t1.P2, t1.P3);
+      Console.WriteLine(t2);
+    }
+  }
 }
