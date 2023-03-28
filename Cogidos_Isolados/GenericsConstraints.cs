e6a7a0676c3bd36964e961bd47fa3301b10a3343
Murilo Muller <murilomuller@protonmail.com>
2023-03-28 18:19:23 -0300

    Clean cap05

--- Cogidos_Isolados/GenericsConstraints.cs
+++ Cogidos_Isolados/GenericsConstraints.cs
@@ -3,42 +3,42 @@ using System.Text;
 
 namespace GenericsConstraints
 {
-	class Program
-	{
-		static void Main()
-		{
-			//Creator<StringBuilder> c1 = new Creator<StringBuilder>();
-			//StringBuilder sb = c1.Create();
+  class Program
+  {
+    static void Main()
+    {
+      //Creator<StringBuilder> c1 = new Creator<StringBuilder>();
+      //StringBuilder sb = c1.Create();
 
-			//Creator<int> c2 = new Creator<int>();
-			//int s = c2.Create();
+      //Creator<int> c2 = new Creator<int>();
+      //int s = c2.Create();
 
-			Creator<MyClass> c3 = new Creator<MyClass>();
-			MyClass mc = c3.Create();
-		}
-	}
+      Creator<MyClass> c3 = new Creator<MyClass>();
+      MyClass mc = c3.Create();
+    }
+  }
 
-	public class Creator<T> where T : class, IInitializable, new()
-	{
-		public T Create()
-		{
-			T obj = new T();
-			obj.Init();
-			return obj;
-		}
-	}
+  public class Creator<T> where T : class, IInitializable, new()
+  {
+    public T Create()
+    {
+      T obj = new T();
+      obj.Init();
+      return obj;
+    }
+  }
 
-	public interface IInitializable
-	{
-		void Init();
-	}
+  public interface IInitializable
+  {
+    void Init();
+  }
 
-	public class MyClass : IInitializable
-	{
-		public void Init()
-		{
-			Console.WriteLine("Inicialização");
-		}
-	}
+  public class MyClass : IInitializable
+  {
+    public void Init()
+    {
+      Console.WriteLine("Inicialização");
+    }
+  }
 
 }
